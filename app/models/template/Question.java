7a2e4dee4dc11db67d4af84a4bcf3cbd06ad2603
adam mao <saalih@qq.com>
2013-12-31 01:01:29 +0800

    rename

---
+++ app/models/template/Question.java
@@ -0,0 +1,15 @@
+package models.template;
+
+/**
+ * Created by mangix on 13-12-29.
+ */
+
+public class Question {
+    public Long questionId;
+    public String context;
+
+    public Question(Long questionId, String context){
+        this.questionId = questionId;
+        this.context = context;
+    }
+}
