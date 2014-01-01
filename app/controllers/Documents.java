e9fff0d8ce976dc22ddf5712e51e801c87b67d3e
mangix <maqh1988@gmail.com>
2014-01-01 12:21:47 +0800

    hh

--- app/controllers/Documents.java
+++ app/controllers/Documents.java
@@ -1,33 +1,33 @@
 package controllers;
 
 import com.aperture.docx.service.DocxService;
+import models.*;
 import models.template.*;
 import play.mvc.*;
 import views.html.dashboard;
 import views.html.documents.*;
-import models.User;
 
 @Security.Authenticated(Secured.class)
 public class Documents extends Controller {
-  
+
     public static Result index() {
         return ok(index.render(models.Module.getDocuments()));
 
     }
 
-    public static Result detail(Long id){
+    public static Result detail(Long id) {
 
-        models.Module document = models.Module.getById(id);
+        String name = models.Module.getById(id).name;
 
-        List<Object> list = DocxService.analyzeModule(document.name);
+        models.template.Module document = DocxService.analyzeModule(name);
 
 //        models.template.Module mockDocument = Data.generate();
 
         return ok(detail.render(document));
 
     }
-    
-    public static Result view(){
+
+    public static Result view() {
         return ok(dashboard.render(User.find.byId(session("email"))));
     }
 
