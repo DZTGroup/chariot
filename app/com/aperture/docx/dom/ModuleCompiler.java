7bb57adc5628fbe7cd29ca5fd96c0f7c6d236e83
adam mao <saalih@qq.com>
2014-01-01 16:14:53 +0800

    fix a bug

--- app/com/aperture/docx/dom/ModuleCompiler.java
+++ app/com/aperture/docx/dom/ModuleCompiler.java
@@ -27,8 +27,8 @@ public class ModuleCompiler {
 	public void save(String path) throws Docx4JException {
 		doc.save(path);
 	}
-	
-	private void pend(Object o){
+
+	private void pend(Object o) {
 		// normal;
 		// if not inside module content, ignore
 		Object oc = XmlUtils.deepCopy(o);
@@ -36,8 +36,8 @@ public class ModuleCompiler {
 		if (oc instanceof ContentAccessor)
 			((ContentAccessor) oc).getContent().clear();
 
-		doc.tail(oc, ((Child) o).getParent() != null ? ((Child) o)
-				.getParent().getClass() : null);
+		doc.tail(oc, ((Child) o).getParent() != null ? ((Child) o).getParent()
+				.getClass() : null);
 	}
 
 	public void pendModule(Module m) throws Docx4JException {
@@ -46,63 +46,69 @@ public class ModuleCompiler {
 		final Module pendingModule = m;
 
 		final BiMap<Module, Object> subs = m.getSubModuleEntry();
-		new TraversalUtil(pendingModule.doc.getBody(), new TraversalUtil.CallbackImpl() {
-			BigInteger wrapperId = null;
-			List<Docx4JException> errors = new ArrayList<Docx4JException>();
+		new TraversalUtil(pendingModule.doc.getBody(),
+				new TraversalUtil.CallbackImpl() {
+					BigInteger wrapperId = null;
+					List<Docx4JException> errors = new ArrayList<Docx4JException>();
 
-			@Override
-			public List<Object> apply(Object o) {
-				if (o instanceof CommentRangeStart) {
-					if (wrapperId == null) {
-						// this is the wrapper of this module
-						wrapperId = ((CommentRangeStart) o).getId();
-					} else {
-						// sub module
-						pend(o);
-						Module sub = subs.inverse().get(o);
-						try {
-							pendModule(sub);
-						} catch (Docx4JException e) {
-							//
-							e.printStackTrace();
-							errors.add(e);
-						}
-					}
-				} else if (o instanceof CommentRangeEnd) {
-					// just pend unless its module ending
-					if (!((CommentRangeEnd) o).getId().equals(wrapperId)) {
-						pend(o);
-					}
-				} else if (o instanceof CommentReference) {
-					// just pend unless its module ending
-					// also, add comment to doc comment parts
-					CommentReference cr = (CommentReference) o;
-					BigInteger id = cr.getId();
-					
-					if (!id.equals(wrapperId)) {
-						// just pend
-						for (Comment c : pendingModule.doc.getComment().getComment()) {
-							if (id.equals(c.getId())) {
-								String text = Docx.extractText(c);
-								doc.createComment(id, text);
-								Object ref = Docx.createRunCommentReference(id);
+					@Override
+					public List<Object> apply(Object o) {
+						if (o instanceof CommentRangeStart) {
+							if (wrapperId == null) {
+								// this is the wrapper of this module
+								wrapperId = ((CommentRangeStart) o).getId();
+							} else {
+								// sub module
+								pend(o);
+								Module sub = subs.inverse().get(o);
+								try {
+									pendModule(sub);
+								} catch (Docx4JException e) {
+									//
+									e.printStackTrace();
+									errors.add(e);
+								}
+							}
+						} else if (o instanceof CommentRangeEnd) {
+							// just pend unless its module ending
+							if (!((CommentRangeEnd) o).getId()
+									.equals(wrapperId)) {
+								pend(o);
+							}
+						} else if (o instanceof CommentReference) {
+							// just pend unless its module ending
+							// also, add comment to doc comment parts
+							CommentReference cr = (CommentReference) o;
+							BigInteger id = cr.getId();
 
-								doc.tail(ref, cr.getParent() != null ? cr
-										.getParent().getClass() : null);
-								
-								break;
+							if (!id.equals(wrapperId)) {
+								// just pend
+								for (Comment c : pendingModule.doc.getComment()
+										.getComment()) {
+									if (id.equals(c.getId())) {
+										String text = Docx.extractText(c);
+										doc.createComment(id, text);
+										Object ref = Docx
+												.createCommentReference(id);
+
+										doc.tail(ref,
+												cr.getParent() != null ? cr
+														.getParent().getClass()
+														: null);
+
+										break;
+									}
+								}
+							}
+						} else {
+							if (wrapperId != null) {
+								pend(o);
 							}
 						}
-					}
-				} else {
-					if (wrapperId != null) {
-						pend(o);
-					}
-				}
 
-				//
-				return null;
-			}
-		});
+						//
+						return null;
+					}
+				});
 	}
 }
