f1506e36938190fbcf3820a588e22c8273b1cce4
JeffreyWang <quanwang06@gmail.com>
2014-01-19 16:57:04 +0800

    login

--- chariot-web/app/controllers/Application.java
+++ chariot-web/app/controllers/Application.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import static play.data.Form.form;
+import models.User;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
@@ -10,5 +13,50 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+    
+ // -- Authentication
+
+ 	public static class Login {
+
+ 		public String email;
+ 		public String password;
+
+ 		public String validate() {
+ 			if (User.authenticate(email, password) == null) {
+ 				return "Invalid user or password";
+ 			}
+ 			return null;
+ 		}
+
+ 	}
+
+ 	/**
+ 	 * Login page.
+ 	 */
+ 	public static Result login() {
+ 		return ok(login.render(form(Login.class)));
+ 	}
+
+ 	/**
+ 	 * Handle login form submission.
+ 	 */
+ 	public static Result authenticate() {
+ 		Form<Login> loginForm = form(Login.class).bindFromRequest();
+ 		if (loginForm.hasErrors()) {
+ 			return badRequest(login.render(loginForm));
+ 		} else {
+ 			session("email", loginForm.get().email);
+ 			return redirect(routes.Application.index());
+ 		}
+ 	}
+
+ 	/**
+ 	 * Logout and clean the session.
+ 	 */
+ 	public static Result logout() {
+ 		session().clear();
+ 		flash("success", "You've been logged out");
+ 		return redirect(routes.Application.login());
+ 	}
 
 }
